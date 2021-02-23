6b3f3def5409807527b87ec07d2181755e340bf2
Jesus Reyes <jesusareyes01@gmail.com>
2021-02-22 23:20:32 -0600

    added react-redux-loading library

--- src/Actions/shared.js
+++ src/Actions/shared.js
@@ -2,17 +2,19 @@ import { getInitialData } from '../Utils/api'
 import { receiveUsers } from '../Actions/users'
 import { receiveTweets } from '../Actions/tweets'
 import { setAuthedUser } from '../Actions/authedUser'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
 const AUTHED_ID = 'tylermcginnis'
 
 export function handleInitialData () {
     return (dispatch) => {
+        dispatch(showLoading())
         return getInitialData()
             .then(({ users, tweets }) => {
                 dispatch(receiveUsers(users))
                 dispatch(receiveTweets(tweets))
                 dispatch(setAuthedUser(AUTHED_ID))
-
+                dispatch(hideLoading())
             })
     }
 }
