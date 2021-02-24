82a1d2a47d431b01de0711d5b4627e97c5790d55
Jesus Reyes <jesusareyes01@gmail.com>
2021-02-24 17:54:43 -0600

    completed TweetPage component which shows selected tweet and allows reply and shows previous replies;

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,6 +4,8 @@ import { handleInitialData } from '../Actions/shared'
 import Dashboard from './Dashboard'
 import LoadingBar from 'react-redux-loading'
 import NewTweet from './NewTweet'
+import TweetPage from './TweetPage'
+
 
 class App extends Component {
   componentDidMount() {
@@ -15,7 +17,7 @@ class App extends Component {
         <LoadingBar />
         {this.props.loading === true
         ?null
-        :<NewTweet/>
+        :<TweetPage match={{params: {id: "2mb6re13q842wu8n106bhk"}}}/>
         }
         
       </div>
