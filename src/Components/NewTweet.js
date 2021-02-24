735c20e5cee613ee81943c44c6f8a5ba2e0276ee
Jesus Reyes <jesusareyes01@gmail.com>
2021-02-24 16:17:47 -0600

    added new action to tweets reducer and added handleAddTweet action to NewTweet component in handleSumbit function

--- src/Components/NewTweet.js
+++ src/Components/NewTweet.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleAddTweet } from '../Actions/tweets'
 
 class NewTweet extends Component {
     state = {
@@ -15,11 +17,10 @@ class NewTweet extends Component {
 
     handleSubmit = (e) => {
         e.preventDefault()
-
         const { text } = this.state
-        //todo add tweet to store
 
-        console.log('New Tweet', text)
+        const { dispatch, id } = this.props
+        dispatch(handleAddTweet(text, id))
 
         this.setState(() => ({
             text: ''
@@ -59,4 +60,4 @@ class NewTweet extends Component {
     }
 }
 
-export default NewTweet
+export default connect()(NewTweet)
